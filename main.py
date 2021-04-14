bf6b099f674a24d1c8e1fd2000ea39a12e06fb7a
mihailalekseev <MkiS1KbImz>
2021-04-15 00:53:08 +0300

    Added tests and comments

--- main.py
+++ main.py
@@ -1,22 +1,58 @@
 from imageai.Detection import ObjectDetection
+import time
 import os
 
+print('[START MODEL LOAD] ' + time.strftime("%d:%m:%Y-%H:%M:%S"))
+
 detector = ObjectDetection()
-detector.setModelTypeAsRetinaNet()
-detector.setModelPath("resnet50_coco_best_v2.1.0.h5")
+
+""" resnet50_coco_best_v2.1.0.h5
+    - модель весит 152 mb
+    - время загрузки модели 3-4 секунды
+    - время процессинга первых изображений 20-30 секунд, после 4го скорость стала составлять около секудны
+    - запуск осуществлялся на macbook pro 2017 i7, 16gb, Radeon Pro 555X 4gb
+"""
+# detector.setModelTypeAsRetinaNet()
+# detector.setModelPath("resnet50_coco_best_v2.1.0.h5")
+# folder = 'done_resnet50/'
+
+
+""" yolo-tiny.h5
+    - модель весит 248 mb
+    - время загрузки модели 2 секунды
+    - время процессинга изображения стабильно 3-4 секунды
+    - приемлимо находит авто, было обнаружено на всех фото, но контур не всегда корректны
+    - запуск осуществлялся на macbook pro 2017 i7, 16gb, Radeon Pro 555X 4gb
+"""
+detector.setModelTypeAsYOLOv3()
+detector.setModelPath("yolo.h5")
+folder = 'done_yolo/'
+
+
+""" yolo.h5
+    - время загрузки модели меньше секунды 
+    - время процессинга изображения стабильно 3-4 секунды
+    - очень плохо находит авто, на 2х из 7 фото авто обнаружено не было
+    - запуск осуществлялся на macbook pro 2017 i7, 16gb, Radeon Pro 555X 4gb
+"""
+# detector.setModelTypeAsTinyYOLOv3()
+# detector.setModelPath("yolo-tiny.h5")
+# folder = 'done_yolotiny/'
+
 detector.loadModel()
 
+print('[END MODEL LOAD] ' + time.strftime("%d:%m:%Y-%H:%M:%S"))
 
 # process image
 directory = 'images'
 file_names = os.listdir(directory)
 file_names.sort()
 for file in file_names:
-    print(file)
+    print('\n [START FILE' + file + ' READ ] ' + time.strftime("%d:%m:%Y-%H:%M:%S"))
     detections = detector.detectObjectsFromImage(input_image="images/" + file,
-                                                 output_image_path="images/done/" + file,
+                                                 output_image_path="images/" + folder + file,
                                                  minimum_percentage_probability=30)
-
+    print('[END FILE' + file + ' READ ] ' + time.strftime("%d:%m:%Y-%H:%M:%S"))
 
 # process video
 # detections = detector.detectObjectsFromVideo(input_image="image/video.mp4",
